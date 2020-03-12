84ff584d7766f14612d334574d89575ff8f955e9
O. Faouzi <oualid.faouzi@hva.nl>
2020-03-12 20:14:05 +0100

    Removed unnecessary files in the project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './CSS/App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Overview from './JS/ScreenComponents/Overview'
+import Refund from './JS/ScreenComponents/Refund'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Overview</Link>
+            </li>
+            <li>
+              <Link to="/refund">Refund</Link>
+            </li>
+          </ul>
+        </nav>
+
+         {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/refund" component={Refund}>
+            <Refund />
+          </Route>
+          <Route path="/" component={Overview}>
+            <Overview />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
