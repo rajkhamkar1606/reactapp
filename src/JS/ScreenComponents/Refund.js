b4fcaaa5e65787b7a51bb487a3dca5e103342c8d
O. Faouzi <oualid.faouzi@hva.nl>
2020-03-12 22:48:36 +0100

    Implemented QR code scanner package and navigation bar with bootstrap

--- src/JS/ScreenComponents/Refund.js
+++ src/JS/ScreenComponents/Refund.js
@@ -1,9 +1,33 @@
 import React, { Component } from 'react'
+import QrReader from 'react-qr-reader'
 
 class Refund extends Component{
-    render(){
-    	return <h2>Refund</h2>;
-    }
+    state = {
+		result: 'No result'
+	}
+
+	handleScan = data => {
+		if (data) {
+	  		this.setState({
+	    		result: data
+	  		})
+		console.log(data)
+		}
+	}
+
+	handleError = err => {
+		console.error(err)
+	}
+
+	render() {
+		return (
+	  		<div>
+	    		<QrReader delay={300} onError={this.handleError} onScan={this.handleScan} style={{ width: '100%' }}/>
+	    		<p>{this.state.result}</p>
+	  		</div>
+		)
+	}
+
 }
 
 export default Refund;
