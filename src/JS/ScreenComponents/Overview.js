ac13680ae7a5fb76ffc4644be3bf788f255c3bd7
O. Faouzi <oualid.faouzi@hva.nl>
2020-10-20 14:02:42 +0200

    Revert "Added two folders to the master branch"
    This reverts commit c84037dedfa50a07b5de641fc29e564554e6f335.

--- src/JS/ScreenComponents/Overview.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-class Overview extends Component{
-    render(){
-    	return <h2>Overview</h2>;
-    }
-}
-
-export default Overview;
--- src/JS/ScreenComponents/Refund.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react'
-
-class Refund extends Component{
-    render(){
-    	return <h2>Refund</h2>;
-    }
-}
-
-export default Refund;
