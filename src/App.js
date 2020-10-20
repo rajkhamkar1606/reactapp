63edb25f9aa03b61b99423a06c5ef74af584a7db
O. Faouzi <oualid.faouzi@hva.nl>
2020-10-20 14:02:40 +0200

    Revert "Further implementation of the sessionstorage"
    This reverts commit b4ce6c6601411ab2e5428ee50c0b813c79857ff0.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,5 @@
 /*
 Created by Oualid Faouzi
-
-NOTE: Session storage is used to store sensitive information about the user.
-Session storage was NOT designed to be used as a secured storage mechanism in a browser.
-If an attacker runs Javascript, they can retrieve all the data stored in the sessionstorage
-and send it off to their own domain. This issue can be solved by integrating a database to store the data.
 */
 
 import React from 'react';
@@ -20,38 +15,38 @@ import Overview from './JS/ScreenComponents/Overview';
 import Refund from './JS/ScreenComponents/Refund';
 
 function App() {
-  sessionStorage.clear();
-  sessionStorage.setItem("numOfInvoicesRegistered", 0);
-  console.log("Session memory cleared.")
-  return (
-    <Router>
-      <div>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
-                <Link className="nav-link" to="/">Overview</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to="/refund">Refund</Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-        <Switch>
-          <Route path="/" exact component={Overview}>
-            <Overview />
-          </Route>
-          <Route path="/refund" exact component={Refund}>
-            <Refund />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
+    //Localstorage is going to be used to store invoices to show in the overview
+    localStorage.clear();
+    console.log("Localstorage cleared.")
+    return (
+        <Router>
+            <div>
+                <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+                    <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">Overview</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/refund">Refund</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+                <Switch>
+                    <Route path="/" exact component={Overview}>
+                        <Overview />
+                    </Route>
+                    <Route path="/refund" exact component={Refund}>
+                        <Refund />
+                    </Route>
+                </Switch>
+            </div>
+        </Router>
+    );
 }
 
 export default App;
