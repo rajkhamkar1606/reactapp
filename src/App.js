e7d63cf6c1dabd2e8128d3a8d45790a903b3316d
O. Faouzi <oualid.faouzi@hva.nl>
2020-10-20 14:02:41 +0200

    Revert "Implemented QR code scanner package and navigation bar with bootstrap"
    This reverts commit b4fcaaa5e65787b7a51bb487a3dca5e103342c8d.

--- src/App.js
+++ src/App.js
@@ -1,43 +1,40 @@
 import React from 'react';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min'
+import './CSS/App.css';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link
 } from "react-router-dom";
-import Overview from './JS/ScreenComponents/Overview';
-import Refund from './JS/ScreenComponents/Refund';
+import Overview from './JS/ScreenComponents/Overview'
+import Refund from './JS/ScreenComponents/Refund'
 
 function App() {
   return (
     <Router>
-        <div>
-            <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/">Overview</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/refund">Refund</Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-            <Switch>
-                <Route path="/" component={Overview}>
-                    <Overview />
-                </Route>
-                <Route path="/refund" component={Refund}>
-                    <Refund />
-                </Route>
-            </Switch>
-        </div>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Overview</Link>
+            </li>
+            <li>
+              <Link to="/refund">Refund</Link>
+            </li>
+          </ul>
+        </nav>
+
+         {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/refund" component={Refund}>
+            <Refund />
+          </Route>
+          <Route path="/" component={Overview}>
+            <Overview />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 }
--- src/JS/ScreenComponents/Refund.js
+++ src/JS/ScreenComponents/Refund.js
@@ -1,33 +1,9 @@
 import React, { Component } from 'react'
-import QrReader from 'react-qr-reader'
 
 class Refund extends Component{
-    state = {
-		result: 'No result'
-	}
-
-	handleScan = data => {
-		if (data) {
-	  		this.setState({
-	    		result: data
-	  		})
-		console.log(data)
-		}
-	}
-
-	handleError = err => {
-		console.error(err)
-	}
-
-	render() {
-		return (
-	  		<div>
-	    		<QrReader delay={300} onError={this.handleError} onScan={this.handleScan} style={{ width: '100%' }}/>
-	    		<p>{this.state.result}</p>
-	  		</div>
-		)
-	}
-
+    render(){
+    	return <h2>Refund</h2>;
+    }
 }
 
 export default Refund;
