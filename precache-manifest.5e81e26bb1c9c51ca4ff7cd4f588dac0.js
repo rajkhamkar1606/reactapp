5ada8c6f0ab418033f5e907ae17178418e695bc5
Raj <raj.sameer.khamkar@scai.fraunhofer.de>
2022-06-25 00:00:27 +0200

    Updates

---
+++ precache-manifest.5e81e26bb1c9c51ca4ff7cd4f588dac0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19e5408c6c361c3f8e79d9f4a2e23cee",
+    "url": "/reactapp/index.html"
+  },
+  {
+    "revision": "e2b4abcb4f07069fbdb9",
+    "url": "/reactapp/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "bbb4501db542acd936c5",
+    "url": "/reactapp/static/css/main.9f29a0f0.chunk.css"
+  },
+  {
+    "revision": "e2b4abcb4f07069fbdb9",
+    "url": "/reactapp/static/js/2.ba06a36d.chunk.js"
+  },
+  {
+    "revision": "a02c2e6156cb6e9e5c30be88d1ba3366",
+    "url": "/reactapp/static/js/2.ba06a36d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbb4501db542acd936c5",
+    "url": "/reactapp/static/js/main.500d03db.chunk.js"
+  },
+  {
+    "revision": "b047f048dcc39c16f789",
+    "url": "/reactapp/static/js/runtime-main.64cdac58.js"
+  }
+]);
